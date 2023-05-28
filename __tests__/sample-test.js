c5accd1273aac8c059b598eefcf161645e5cdc6a
Eric T Grubaugh <eric@stoic.software>
2023-05-27 22:49:40 -0600

    Restored broken test

--- __tests__/sample-test.js
+++ __tests__/sample-test.js
@@ -12,8 +12,7 @@ describe('Basic jest test with simple assert', () => {
 	it('should assert strings are equal', () => {
 		const a = 'foobar';
 		const b = 'foobar';
-		// expect(a).toMatch(b);
-    expect(true).toBeFalsy();
+		expect(a).toMatch(b);
 	});
 });
 
